ff3515fc3fca5b17c82f230e6ff299a50ede70bd
Zach Hickman <77699600+Zacharyflopguy@users.noreply.github.com>
2025-01-25 17:01:34 -0600

    Assets

---
+++ Bubble_Broker/Assets/Zach/Scripts/BubblesManager.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubblesManager : MonoBehaviour
+{
+    public List<string> bubbleTexts;
+
+    public void OnBubblesClick()
+    {
+        
+    }
+}
