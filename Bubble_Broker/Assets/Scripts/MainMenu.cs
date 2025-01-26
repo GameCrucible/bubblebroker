e11421371d8cd9efe704ec9282fe541e8167c455
ButtersBo <matthew.sanstra@gmail.com>
2025-01-26 02:40:56 -0600

    main menu

---
+++ Bubble_Broker/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject CanvasMain;
+    public GameObject CanvasTutorial1;
+    public GameObject CanvasTutorial2;
+    public GameObject CanvasTutorial3;
+    public GameObject NextButton;
+
+    public int tutorialStep = 0;
+
+
+    public void startGame()
+    {
+        CanvasMain.SetActive(false);
+        CanvasTutorial1.SetActive(true);
+        NextButton.SetActive(true);
+    }
+
+    public void tutorial()
+    {
+        if (tutorialStep == 0)
+        {
+            CanvasTutorial1.SetActive(false);
+            CanvasTutorial2.SetActive(true);
+            NextButton.SetActive(true);
+            tutorialStep++;
+        }
+        else if (tutorialStep == 1)
+        {
+            CanvasTutorial2.SetActive(false);
+            CanvasTutorial3.SetActive(true);
+            NextButton.SetActive(true);
+            tutorialStep++;
+        }
+        else if (tutorialStep == 2)
+        {
+            CanvasTutorial3.SetActive(false);
+            SceneManager.LoadScene("Office");
+        }
+        
+    }
+
+}
